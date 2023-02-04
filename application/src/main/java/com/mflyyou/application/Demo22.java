eac9336ce6f88a140a4bc172bea0f75218927a19
zhangpanqin <zhangpanqin@outlook.com>
2023-02-05 01:22:02 +0800

    1111

--- application/src/main/java/com/mflyyou/application/Demo22.java
+++ application/src/main/java/com/mflyyou/application/Demo22.java
@@ -1,10 +1,11 @@
 package com.mflyyou.application;
 
+import com.mflyyou.util.DemoUtils;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Demo22 {
     public static void main(String[] args) {
-        log.info("11");
+        System.out.println(new DemoUtils().test());
     }
 }
