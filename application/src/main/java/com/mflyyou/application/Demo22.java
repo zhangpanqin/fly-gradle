0efa98a1ebf67876000a13f9f35ea8cf50aa8a08
zhangpanqin <zhangpanqin@outlook.com>
2023-02-05 00:55:47 +0800

    aaa

---
+++ application/src/main/java/com/mflyyou/application/Demo22.java
@@ -0,0 +1,10 @@
+package com.mflyyou.application;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Demo22 {
+    public static void main(String[] args) {
+        log.info("11");
+    }
+}
---
+++ application/src/test/java/com/mflyyou/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mflyyou.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
