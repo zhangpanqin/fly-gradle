0ef479482029b7e7b3eab531dd0c5cd5b3a6db1e
zhangpanqin <zhangpanqin@outlook.com>
2023-02-05 01:53:19 +0800

    222

--- application/src/main/java/com/mflyyou/application/Demo22.java
+++ application/src/main/java/com/mflyyou/application/Demo22.java
@@ -2,10 +2,15 @@ package com.mflyyou.application;
 
 import com.mflyyou.util.DemoUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
 @Slf4j
 public class Demo22 {
     public static void main(String[] args) {
         System.out.println(new DemoUtils().test());
+        System.out.println(new DemoUtils().test1());
+        System.out.println(new DemoUtils().test3());
+        System.out.println(StringUtils.isEmpty("aa"));
+//         Lists.newArrayList("不能使用 google Lists");
     }
 }
--- common/src/main/java/com/mflyyou/util/DemoUtils.java
+++ common/src/main/java/com/mflyyou/util/DemoUtils.java
@@ -1,7 +1,20 @@
 package com.mflyyou.util;
 
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+
 public class DemoUtils {
     public boolean test() {
         return true;
+    }
+
+    public List<String> test1() {
+        return Lists.newArrayList("aaa");
+    }
+
+    public String test3() {
+        return StringUtils.join("aa", "bb");
     }
 }
