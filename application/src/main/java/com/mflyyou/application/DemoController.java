12895b573708702726268170d0015fb90f2c026e
zhangpanqin <zhangpanqin@outlook.com>
2023-03-11 21:00:44 +0800

    完善代码

--- application/src/main/java/com/mflyyou/application/DemoController.java
+++ application/src/main/java/com/mflyyou/application/DemoController.java
@@ -1,12 +1,53 @@
 package com.mflyyou.application;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.jackson.Jacksonized;
+import org.javamoney.moneta.Money;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
 
 @RestController
 public class DemoController {
     @GetMapping("/demo")
     public String demo() {
         return "demo";
+    }
+
+    @PostMapping("/dc-3512")
+    public DcPayload dc3512(@RequestBody DcPayload payload) {
+        return payload;
+    }
+
+    @Getter
+    @Builder
+    @Jacksonized
+    public static class DcPayload implements Serializable{
+        @Serial
+        private static final long serialVersionUID = -8859377635691053219L;
+        private List<PayoutFlowItem> payoutFlows;
+
+        @Getter
+        @Builder
+        @Jacksonized
+        public static class PayoutFlowItem implements Serializable {
+            @Serial
+            private static final long serialVersionUID = 1L;
+
+            private String settlementId;
+
+            private String sourceAccountId;
+
+            private String destinationAccountId;
+
+            private Money amount;
+        }
     }
 }
---
+++ application/src/main/java/com/mflyyou/application/JacksonConfig.java
@@ -0,0 +1,22 @@
+package com.mflyyou.application;
+
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.zalando.jackson.datatype.money.MoneyModule;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> {
+            builder.serializerByType(Long.class, ToStringSerializer.instance);
+        };
+    }
+
+    @Bean
+    public MoneyModule javaTimeModule() {
+        return new MoneyModule();
+    }
+}
